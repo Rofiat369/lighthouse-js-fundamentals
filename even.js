34d8ac5ee7b0c2fb811729c30b9d243ea57cbc1d
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-11 05:18:55 +0000

    Understanding how modulo is used to check if isEven

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const thirteenNotEven = isEven(13);
+const sixeven = isEven(6);
+
+console.log(thirteenNotEven);
+console.log(sixeven);
