46550400b2fd807caa5eb9e366c4378742303f0d
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-11 17:40:34 +0000

    Used different array properties to solve a question

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours);
+
+let firstFlavor = iceCreamFlavours[0];
+console.log(firstFlavor);
+
+let lastFlavor = iceCreamFlavours[5];
+console.log(lastFlavor);
+
+let allFlavor = iceCreamFlavours.length;
+console.log(allFlavor);
