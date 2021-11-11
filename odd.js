9084d4179703caec4507925d8ad23d25534defa9
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-11 17:41:45 +0000

    used the if/else statement to do some math

--- odd.js
+++ odd.js
@@ -1,6 +1,43 @@
-const isOdd = function (num){
-  return num % 3 === 0;
-} 
+// const isOdd = function (num){
+//   return num % 3 === 0;
+// } 
 
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
+// console.log("3 is odd: " + isOdd(3));
+// console.log("8 is odd: " + isOdd(8));
+
+function calculateRectangleArea(length, width){
+  if (length < 0 || width < 0){
+    return (undefined);
+  } else {
+    return length * width;
+  }
+}
+
+function calculateTriangleArea(base, height){
+  if (base < 0 || height < 0) {
+    return undefined;
+  } else {
+    return base * height /2;
+  } 
+}
+
+  function calculateCircleArea(radius){
+    if (radius < 0){
+      return undefined;
+    } else {
+      return Math.PI * radius *radius;
+    }
+  }
+
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
